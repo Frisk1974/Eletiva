79af350a3e132a5c2f470089cc6778c2f06bb872
Kyrell-Einar <heronaantonio@gmail.com>
2025-05-29 12:18:19 -0300

    teste

---
+++ script.js
@@ -0,0 +1,3 @@
+ScrollReveal().reveal('.titulo', { delay: 100, distance: '50px', origin: 'top' });
+ScrollReveal().reveal('.subtitulo', { delay: 300, distance: '50px', origin: 'bottom' });
+ScrollReveal().reveal('.btn-entrar', { delay: 500, scale: 0.8 });
